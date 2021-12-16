0976033d503f6e4093f0c258699b2024d3aa0141
halo <zhaohailong@88.com>
2021-12-16 16:34:53 +0800

    nacos config

---
+++ nacos-spring-cloud-example/nacos-spring-cloud-config-example/src/main/java/com/halo/nacos/example/spring/cloud/NacosConfigApplication.java
@@ -0,0 +1,17 @@
+package com.halo.nacos.example.spring.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Description
+ * @Author halo
+ * @Date 2021/12/16
+ */
+@SpringBootApplication
+public class NacosConfigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NacosConfigApplication.class);
+    }
+}
---
+++ nacos-spring-cloud-example/nacos-spring-cloud-config-example/src/main/java/com/halo/nacos/example/spring/cloud/controller/ConfigController.java
@@ -0,0 +1,25 @@
+package com.halo.nacos.example.spring.cloud.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Description
+ * @Author halo
+ * @Date 2021/12/16
+ */
+@RestController
+@RequestMapping(value = "/config")
+@RefreshScope
+public class ConfigController {
+
+    @Value("${useLocalCache:false}")
+    private boolean useLocalCache;
+
+    @RequestMapping("/get")
+    public boolean get() {
+        return useLocalCache;
+    }
+}
