37406d5707cdb2c5cdf4a85ace354ee64f0eac7f
halo <zhaohailong@88.com>
2021-12-16 10:48:13 +0800

    Initial commit

---
+++ nacos-spring-cloud-example/nacos-spring-cloud-discovery-example/nacos-spring-cloud-provider-example/src/main/java/com/halo/nacos/example/spring/cloud/NacosProviderApplication.java
@@ -0,0 +1,31 @@
+package com.halo.nacos.example.spring.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Description
+ * @Author halo
+ * @Date 2021/12/15
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class NacosProviderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NacosProviderApplication.class);
+    }
+
+    @RestController
+    class EchoController {
+        @RequestMapping(value = "/echo/{string}", method = RequestMethod.GET)
+        public String echo(@PathVariable String string) {
+            return "Hello Nacos Discovery " + string;
+        }
+    }
+}
